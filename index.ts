5a9bb3533cae7d3dfd4f0f0a8b3805c53f4c281c
Sergey Shandar <sergey.shandar@gmail.com>
2018-12-05 16:46:19 -0800

    0.0.4

--- index.ts
+++ index.ts
@@ -3,36 +3,36 @@ export type Tuple0 = ReadonlyArray<never>
 export const tuple0: Tuple0 = []
 
 export interface Tuple1<T> extends Iterable<T> {
+    readonly length: 1
     readonly [0]: T
-    readonly [1]?: never
 }
 
 export const tuple1 = <T>(v: T): Tuple1<T> => [v]
 
 export interface Tuple2<T0, T1> extends Iterable<T0 | T1> {
+    readonly length: 2
     readonly [0]: T0
     readonly [1]: T1
-    readonly [2]?: never
 }
 
 export const tuple2 = <T0, T1>(v0: T0, v1: T1): Tuple2<T0, T1> => [v0, v1]
 
 export interface Tuple3<T0, T1, T2> extends Iterable<T0 | T1 | T2> {
+    readonly length: 3
     readonly [0]: T0
     readonly [1]: T1
     readonly [2]: T2
-    readonly [3]?: never
 }
 
 export const tuple3 = <T0, T1, T2>(v0: T0, v1: T1, v2: T2): Tuple3<T0, T1, T2> => [v0, v1, v2]
 
 // For example ["null", "string", "boolean", "integer"]
 export interface Tuple4<T0, T1, T2, T3> extends Iterable<T0 | T1 | T2 | T3> {
+    readonly length: 4
     readonly [0]: T0
     readonly [1]: T1
     readonly [2]: T2
     readonly [3]: T3
-    readonly [4]?: never
 }
 
 export const tuple4 = <T0, T1, T2, T3>(v0: T0, v1: T1, v2: T2, v3: T3): Tuple4<T0, T1, T2, T3> =>
@@ -40,12 +40,12 @@ export const tuple4 = <T0, T1, T2, T3>(v0: T0, v1: T1, v2: T2, v3: T3): Tuple4<T
 
 // For example ["null", "string", "boolean", "integer", "number"]
 export interface Tuple5<T0, T1, T2, T3, T4> extends Iterable<T0 | T1 | T2 | T3 | T4> {
+    readonly length: 5
     readonly [0]: T0
     readonly [1]: T1
     readonly [2]: T2
     readonly [3]: T3
     readonly [4]: T4
-    readonly [5]?: never
 }
 
 export const tuple5 = <T0, T1, T2, T3, T4>(
@@ -55,13 +55,13 @@ export const tuple5 = <T0, T1, T2, T3, T4>(
 
 // For example ["null", "string", "boolean", "integer", "number", "array"]
 export interface Tuple6<T0, T1, T2, T3, T4, T5> extends Iterable<T0 | T1 | T2 | T3 | T4 | T5> {
+    readonly length: 6
     readonly [0]: T0
     readonly [1]: T1
     readonly [2]: T2
     readonly [3]: T3
     readonly [4]: T4
     readonly [5]: T5
-    readonly [6]?: never
 }
 
 export const tuple6 = <T0, T1, T2, T3, T4, T5>(
@@ -73,6 +73,7 @@ export const tuple6 = <T0, T1, T2, T3, T4, T5>(
 export interface Tuple7<T0, T1, T2, T3, T4, T5, T6> extends
     Iterable<T0 | T1 | T2 | T3 | T4 | T5 | T6>
 {
+    readonly length: 7
     readonly [0]: T0
     readonly [1]: T1
     readonly [2]: T2
@@ -80,7 +81,6 @@ export interface Tuple7<T0, T1, T2, T3, T4, T5, T6> extends
     readonly [4]: T4
     readonly [5]: T5
     readonly [6]: T6
-    readonly [7]?: never
 }
 
 export const tuple7 = <T0, T1, T2, T3, T4, T5, T6>(
